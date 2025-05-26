69328df9b284b774c22545484208e4f9d1833c3e
chiba <edyzrg1118@gmail.com>
2025-05-26 23:37:11 +0900

    Devise対応のためのコントローラとレイアウトを調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base # すべてのコントローラの親クラス。アプリ全体の共通処理を書く場所。
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
+  helper_method :current_user, :user_signed_in? # #メソッドをビューでも使えるようにする
+  before_action :set_locale # #日本語表示されなかったため、対応
+  before_action :configure_permitted_parameters, if: :devise_controller? # #Deviseがコントローラとして動作しているとき（ログイン・新規登録など）だけ、configure_permitted_parameters メソッドを実行するように設定。
+
+  protected # #コントローラーの中で使用
+
+  def set_locale # #日本語表示されなかったため、対応
+    I18n.locale = :ja
+  end
+
+  def configure_permitted_parameters # #許可するパラメータを追加設定するためのメソッド
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ]) # #新規登録（sign_up）時に、:nameパラメータを受け入れる
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
+  end
 end
