79645916aed940a1c37b130427f29c781ee46eda
chiba <edyzrg1118@gmail.com>
2025-06-02 14:07:29 +0900

    FullCalendarに日付クリック時のAjax取得処理を追加

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,13 +1,38 @@
-document.addEventListener("DOMContentLoaded", function () { //HTMLの読み込み後、関数を実行
-    const el = document.getElementById("calendar"); //idのcalendar要素をelに代入
-    if ( !el || typeof FullCalendar === "undefined") { //elがない時、フルカレンダーがない時
-        return; //処理を中断
+$(function () { //ページの読み込みが完了したとき中の処理を実行する
+    const el = document.getElementById("calendar"); //elが見つからなかった、fullcalendarが読み込まれない場合
+    if (!el || typeof FullCalendar === "undefined") { 
+        return; //処理終了
     }
 
-    const calendar = new FullCalendar.Calendar(el, {
+    const calendar = new FullCalendar.Calendar(el, { //新しいカレンダーを作成
         initialView: "dayGridMonth", //月表示
-        locale: "ja" //日本語
-    });
-    calendar.render(); //描画
-})
+        locale: "ja", //日本語
+        dateClick: function(info) { //日付をクリックしたときに呼ばれる関数
+          const clickDate = info.dateStr //文字列が入り
+          console.log("クリックされた日付:", clickDate); //出力
 
+    // ajax,getリクエスト
+    $.ajax({ //clickDateをパラメータとして/visits/by_dateにGETリクエストを送る
+        url: "/visits/by_date", //railsのコントローラーに送る
+        type: "GET", //getリクエスト
+        data: { date: clickDate },
+        dataType: "html",
+        success: function (data) {
+          $("#schedule-list").html(data); //サーバーからのレスポンスが成功したら#schedule-list要素の中身を書き換える（取得した予定のHTMLで）
+        },
+        error: function () {
+          $("#schedule-list").html("<p class='flash-error text-center'>予定の取得ができません。</p>");
+        }
+      });
+
+    //   axios.post("/logs", {
+    //     event: "click",
+    //     date: clickDate
+    //   }).then(response => {
+    //     console.log("記録されました")
+    //   })
+    // axiosを使ってみたかったが、一旦ストップ
+    }
+    });
+  calendar.render();
+});
