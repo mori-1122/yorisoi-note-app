d2e605db1558425d6c11e90d3f9a7defad64817b
h3pei <hs.iarn3@gmail.com>
2025-06-08 14:17:49 +0900

    axios も不要そうなので一旦削除

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,3 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.bundle.min.js"
-pin "axios" # @1.9.0
-pin "#lib/adapters/http.js", to: "#lib--adapters--http.js.js" # @1.9.0
-pin "#lib/platform/node/classes/FormData.js", to: "#lib--platform--node--classes--FormData.js.js" # @1.9.0
-pin "#lib/platform/node/index.js", to: "#lib--platform--node--index.js.js" # @1.9.0
