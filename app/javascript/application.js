1d25125c7b6a2ab20fe62ec7b7458a1bb3401864
chiba <edyzrg1118@gmail.com>
2025-06-10 11:31:36 +0900

    不要なライブラリのimportを削除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "controllers"
 import * as Turbo from "@hotwired/turbo-rails"
 window.Turbo = Turbo
-import "controllers"
-import * as bootstrap from "bootstrap"
-import jquery from "jquery"
-window.$ = jquery
-import Rails from "@rails/ujs"
-Rails.start()
