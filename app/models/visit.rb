0ac545926063497c76864a141bf2fde137d23e96
chiba <edyzrg1118@gmail.com>
2025-06-11 13:46:08 +0900

    バリデーション追加：予約日時 appointed_at を必須項目として未来日時のみ許可

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -5,5 +5,17 @@ class Visit < ApplicationRecord
   has_many :documents, dependent: :destroy
   has_many :recordings, dependent: :destroy
 
-  validates :visit_date, :hospital_name, :purpose, presence: true
+  # 必須項目のバリデーション
+  validates :visit_date, :hospital_name, :purpose, :appointed_at, presence: true
+
+  # 予約時間は未来でなければならない
+  validate :appointed_at_must_be_in_the_future
+
+  private
+
+  def appointed_at_must_be_in_the_future
+    if appointed_at.present? && appointed_at < Time.current
+      errors.add(:appointed_at, "は今日より後の日時を指定してください")
+    end
+  end
 end
