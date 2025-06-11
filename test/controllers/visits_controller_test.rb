6bcabace4d3cec3fe6629d3185ed3d9fdf812881
chiba <edyzrg1118@gmail.com>
2025-06-11 09:38:24 +0900

    test: Deviseログイン対応とルートURL修正に伴うコントローラーテスト修正

--- test/controllers/visits_controller_test.rb
+++ test/controllers/visits_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class VisitsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
   test "should get index" do
     get visits_url
     assert_response :success
