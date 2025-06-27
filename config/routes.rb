50d6358375038f5a4cf8521e783d45d560095616
chiba <edyzrg1118@gmail.com>
2025-06-27 13:04:29 +0900

    質問テンプレート選択・検索・保存用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "question_selections/create"
+  get "questions/select"
+  get "questions/search"
   devise_for :users # #ユーザーのログイン・登録などに必要なルート
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "home#index" # #トップページ（/）にアクセス
@@ -9,4 +12,13 @@ Rails.application.routes.draw do
       get "by_date"
     end
   end
+
+  resources :questions, only: [] do
+    collection do
+      get :select
+      get :search
+    end
+  end
+
+  resources :question_selections, only: [:create]
 end
