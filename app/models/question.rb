4f19a3369a6137011dcbe6b230e5a4b40dd2fc0b
chiba <edyzrg1118@gmail.com>
2025-06-29 22:08:38 +0900

    質問モデルのバリデーションおよび関連制約を強化（複合ユニーク制約・削除制限の追加）

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,12 @@
 class Question < ApplicationRecord # #Questionモデルを定義
-  belongs_to :department, optional: true # #一つの診療科に属す
-  belongs_to :question_category, optional: true # この質問は1つのカテゴリ(question_category)に属する
-  has_many :question_selections, dependent: :destroy # ユーザーの選択履歴と関連、質問削除時に選択も削除
+  belongs_to :department, optional: true # #department（診療科）に属することを示しつつ、optional: trueによって、診療科が未設定でも保存可能
+  belongs_to :question_category, optional: true # 質問は question_category（カテゴリ）にも属す。任意であり、カテゴリがなくても保存可能とする。
+  has_many :question_selections, dependent: :restrict_with_exception # #この質問が選択履歴として使われている場合、削除しようとすると例外を発生させて削除できない。
 
   validates :content, presence: true # #質問内容（content）は必須。空の場合は保存できない
-  validates :content, uniqueness: { message: "は、同じ診療科、カテゴリにすでにあります" }
+  validates :content, uniqueness: {# #同じカテゴリ(question_category_id)と診療科(department_id)の組み合わせにおいて一意でなければならない
+    scope: [ :question_category_id, :department_id ], # #カテゴリまたは診療科が異なれば、内容が重複しても登録可能
+    message: "は、同じ診療科、カテゴリにすでにあります" }
   validate :department_or_category_must_be_present # #独自のバリデーション 診療科（department）もカテゴリ（question_category）も両方が空だと保存できない アプリケーションレベルのチェック
 
   private
