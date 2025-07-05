953ef301a81be6f2d020a951936b08e8a261c41d
chiba <edyzrg1118@gmail.com>
2025-07-06 06:07:50 +0900

    Questionモデル: 診療科・カテゴリ単位での質問の重複を防止するバリデーションを追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,9 +3,11 @@ class Question < ApplicationRecord # #Questionモデルを定義
   belongs_to :question_category, optional: true # この質問は1つのカテゴリ(question_category)に属する
   has_many :question_selections, dependent: :destroy # ユーザーの選択履歴と関連、質問削除時に選択も削除
 
-  validates :content, presence: true # #質問内容（content）は必須。空の場合は保存できない
-  validates :content, uniqueness: { message: "は、同じ診療科、カテゴリにすでにあります" }
+  validates :content, presence: true # 　質問内容（content）は必須。空の場合は保存できない
+  validates :content, uniqueness: { scope: [ :department_id, :question_category_id ], message: "は、同じ診療科、カテゴリにすでにあります" }
   validate :department_or_category_must_be_present # #独自のバリデーション 診療科（department）もカテゴリ（question_category）も両方が空だと保存できない アプリケーションレベルのチェック
+
+  # 検索フィルターは、また後で
 
   private
 
