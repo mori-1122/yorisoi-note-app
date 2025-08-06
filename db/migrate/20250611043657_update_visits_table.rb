c0f2ffc89464e5074030b3741319c41282328b07
Chiba <chi.11220214.yo@gmail.com>
2025-08-07 03:00:23 +0900

    visitsテーブルのマイグレーション整理とschema.rbのバージョン修正

--- db/migrate/20250611043657_update_visits_table.rb
+++
@@ -1,6 +0,0 @@
-class UpdateVisitsTable < ActiveRecord::Migration[8.0]
-  def change
-    remove_column :visits, :doctor_name, :string
-    add_column :visits, :appointed_at, :datetime, null: false
-  end
-end
