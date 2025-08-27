652c12b0a7e1bdb5619548746e8007b45fd5bb13
Chiba <chi.11220214.yo@gmail.com>
2025-08-28 03:06:29 +0900

    DocumentsControllerを追加し通院に紐づく画像のCRUDを実装

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,57 @@
+class DocumentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_visit
+  before_action :set_document, only: [ :edit, :update, :destroy ]
+
+  def index
+    @documents = @visit.documents.includes(:user)
+  end
+
+  def new
+    @document = @visit.documents.build
+  end
+
+  def create
+    @document = @visit.documents.build(document_params)
+    @document.user = current_user
+
+    if @document.save
+      flash[:notice] = "画像をアップロードしました。"
+      redirect_to visit_documents_path(@visit)
+    else
+      flash.now[:error] = "アップロードに失敗しました。"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @document.update(document_params)
+      redirect_to visit_documents_path(@visit), notice: "画像を更新しました。"
+    else
+      flash.now[:error] = "更新に失敗しました。"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @document.destroy
+    redirect_to visit_documents_path(@visit), notice: "画像を削除しました。"
+  end
+
+  private
+
+  def set_visit
+    @visit = current_user.visits.find(params[:visit_id])
+  end
+
+  def document_params
+    params.require(:document).permit(:image, :doc_type)
+  end
+
+  def set_document
+    @document = @visit.documents.find(params[:id])
+  end
+end
