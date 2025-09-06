7606877d2b0ddab002d3fa7f991f7d69bfd8c701
Chiba <chi.11220214.yo@gmail.com>
2025-09-06 20:05:47 +0900

    メールを送れるようにする

--- app/mailers/visit_mailer.rb
+++ app/mailers/visit_mailer.rb
@@ -1,2 +1,10 @@
 class VisitMailer < ApplicationMailer
+  def reminder
+    @visit = params[:visit]
+    @user = @visit.user
+    mail(
+      to:
+      @user.email,
+      subject: "【リマインダー】#{@visit.hospital_name}の受診予定")
+  end
 end
