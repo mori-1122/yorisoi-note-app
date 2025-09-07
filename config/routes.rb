26f54ac61eacab92cf898c00908649f3ca063e1c
Chiba <chi.11220214.yo@gmail.com>
2025-09-07 11:15:32 +0900

    routes.rbのクォートを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users # ユーザーのログイン・登録などに必要なルート
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "home#index"
