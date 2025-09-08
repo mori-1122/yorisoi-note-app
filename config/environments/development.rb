97770b98d862b04270d96faee5d7c8d26b4830be
Chiba <chi.11220214.yo@gmail.com>
2025-09-08 20:28:32 +0900

    開発環境でのSMTP設定をコメントアウトしletter_opener_webを利用

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,15 +40,16 @@ Rails.application.configure do
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 5000 }
   config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    domain: "gmail.com",
-    port: 587,
-    user_name: ENV["MAILER_SENDER"],
-    password: ENV["MAILER_PASSWORD"],
-    authentication: "plain",
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.smtp_settings =
+  # {
+  #   address: "smtp.gmail.com",
+  #   domain: "gmail.com",
+  #   port: 587,
+  #   user_name: ENV["MAILER_SENDER"],
+  #   password: ENV["MAILER_PASSWORD"],
+  #   authentication: "plain",
+  #   enable_starttls_auto: true
+  # }　開発ではコメントアウトする
 
   # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
