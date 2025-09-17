17f69e17cf55a3c6bdc2a1a27daa9b039221ad49
Chiba <chi.11220214.yo@gmail.com>
2025-09-17 12:50:56 +0900

    UserモデルにProfile関連を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :question_selections, dependent: :destroy
   has_many :notifications, dependent: :destroy
 
+  has_one :profile, dependent: :destroy
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :encrypted_password, presence: true
