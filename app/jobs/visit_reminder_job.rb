3e3cf33b881180138f901e41247284ce43abad30
Chiba <chi.11220214.yo@gmail.com>
2025-09-23 04:50:59 +0900

    VisitReminderJobを通知単位で処理し、トランザクション内で送信と更新を実行するよう変更

--- app/jobs/visit_reminder_job.rb
+++ app/jobs/visit_reminder_job.rb
@@ -2,13 +2,13 @@ class VisitReminderJob < ApplicationJob
   queue_as :default
 
   def perform
-    # 翌日が受診予定の visit に紐づく通知を探してメール送信
-    visits = Visit.where(visit_date: Time.zone.tomorrow).includes(:notifications)
-    visits.find_each do |visit|  # ← Visitごとに処理
-      notification = visit.notifications.find_by(is_sent: false) # 「未送信の通知」を1件探す
+    target_date = Time.zone.tomorrow
 
-      if notification
-        NotificationMailer.visit_reminder(notification).deliver_later
+    Notification.includes(:visit)
+                .where(is_sent: false, due_date: target_date)
+                .find_each do |notification|
+      Notification.transaction do # トランザクション開始
+        NotificationMailer.visit_reminder(notification).deliver_now
         notification.update!(is_sent: true)
       end
     end
