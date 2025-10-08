f3a7f1f40f7991d6331ed608c01a34af8e46a4f6
Chiba <chi.11220214.yo@gmail.com>
2025-10-09 00:36:53 +0900

    VisitsHelperに日付・時刻・病院名を整形表示するメソッドを追加

--- app/helpers/visits_helper.rb
+++ app/helpers/visits_helper.rb
@@ -1,2 +1,18 @@
 module VisitsHelper
+  # 日時を2025年１０月９日形式で表示
+  def format_visit_date(date)
+    return "未定" if date.blank?
+    date.strftime("%Y年%-m月%-d日")
+  end
+
+  # 　時刻をHH:MM形式で表示
+  def format_appointed_time(time)
+    return "--:--" if time.blank?
+    time.strftime("%H:%M")
+  end
+
+  # 病院名表示
+  def display_hospital_name(name)
+    name.presence || "(未登録)"
+  end
 end
