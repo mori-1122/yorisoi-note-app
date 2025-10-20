8b03fc9eef47784c8821eb200988ce564f1894ae
Chiba <chi.11220214.yo@gmail.com>
2025-10-20 23:24:07 +0900

    Action Mailer設定を本番ドメイン(yorisoi-note.com)に更新

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,21 +58,19 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
-
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   user_name: "apikey",
   password: ENV["SENDGRID_API_KEY"],
-  domain: "heroku.com", # 将来独自ドメイン
+  domain: "yorisoi-note.com",
   address: "smtp.sendgrid.net",
   port: 587,
   authentication: :plain,
   enable_starttls_auto: true
 }
 
-  config.action_mailer.default_url_options = { host: "yorisoi-note-app.herokuapp.com", protocol: "https" }
+  config.action_mailer.default_url_options = { host: "yorisoi-note.com", protocol: "https" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
